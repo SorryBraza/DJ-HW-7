b9e6053c2c7c05e0c86942b17765f89be625325b
Dorzh Dorzhiev <ddr.03@ya.ru>
2024-03-02 19:22:12 +0300

    Initial commit

---
+++ api_with_restrictions/advertisements/filters.py
@@ -0,0 +1,15 @@
+import django_filters
+from django_filters import rest_framework as filters
+
+from advertisements.models import Advertisement
+
+
+class AdvertisementFilter(filters.FilterSet):
+    """Фильтры для объявлений."""
+
+    # TODO: задайте требуемые фильтры
+    created_at = django_filters.DateFromToRangeFilter()
+
+    class Meta:
+        model = Advertisement
+        fields = ['created_at', 'status', 'creator']
